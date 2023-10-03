2c90ea6d2bcdb41e93e738b91f5e137ae281b720
ecee <you@example.com>
2023-10-03 20:32:20 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function submitData (name ,email){
+fetch('http://localhost:3000/users', {
+    method:'post',
+    headers:{
+        'content-type':'application/json',
+        Accept:'application/json',
+  },
+    body:JSON.stringify(data)
+})
+.then(rep => rep.json)
+.then(data =>console.log(data))
+};
+  
+.catch(error => {
+    console.log('error', error)
+    alert error;
+});
